83b2269882e61c243100ef235139391d7106ea8e
AlexOdnov <alex.odnoralov@yandex.ru>
2021-05-05 22:25:33 +0300

    base setup

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
+import { createApp } from 'vue';
+import App from './App.vue';
+import './registerServiceWorker';
+import './assets/normalize.scss';
 
-createApp(App).mount("#app");
+createApp(App).mount('#app');
