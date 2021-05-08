3cf0ff78173ec38a463bd96276c3a804d6c41f61
AlexOdnov <alex.odnoralov@yandex.ru>
2021-05-08 23:18:04 +0300

    add markup for TextOutput component

---
+++ src/components/TextOutput.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="text-output">
+    <p>
+      {{ recognizedText }}
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'TextOutput',
+
+  props: {
+    recognizedText: {
+      type: String,
+      required: true,
+      default: '',
+    },
+  },
+
+  // setup() {},
+});
+</script>
+
+<style lang="scss" scoped>
+.text-output {
+  display: block;
+  padding: var(--outer-padding);
+  overflow: auto;
+  text-align: left;
+  background: var(--bgLightColor);
+  border: 2px solid var(--color);
+  border-radius: var(--outer-padding);
+  scrollbar-width: none;
+
+  &::-webkit-scrollbar {
+    width: 0;
+  }
+}
+</style>
