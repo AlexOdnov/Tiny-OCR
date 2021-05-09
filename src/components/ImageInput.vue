3c5652a1dab3b86a7ed304f1c73719bd1cb7f9d9
AlexOdnov <alex.odnoralov@yandex.ru>
2021-05-09 21:43:25 +0300

    implement image uploading in ImageInput component

--- src/components/ImageInput.vue
+++ src/components/ImageInput.vue
@@ -1,22 +1,71 @@
 <template>
   <label class="image-input">
-    <span class="image-input__label">Загрузить файл</span>
-    <input class="image-input__input visually-hidden" type="file" />
+    <img class="image-input__preview" :src="imageSrc" v-show="imageSrc" />
+    <span
+      class="image-input__wrapper"
+      :class="{ 'image-input__wrapper--with-image': imageSrc }"
+    >
+      <span class="image-input__label">Загрузить файл</span>
+    </span>
+    <input
+      class="image-input__input visually-hidden"
+      type="file"
+      accept="image/*"
+      ref="imageInput"
+      @change.prevent="loadImage"
+    />
   </label>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 
 export default defineComponent({
   name: 'ImageInput',
 
-  // setup() {},
+  emits: {
+    uploadImage: (payload) => (typeof payload === 'string' ? true : false),
+  },
+
+  setup(_, context) {
+    const imageInput = ref<null | HTMLInputElement>(null);
+    const imageSrc = ref<null | string>(null);
+
+    const loadImage = () => {
+      if (imageInput.value?.files?.length) {
+        const file = imageInput.value.files[0];
+
+        if (!file.type.includes('image/')) {
+          console.error('файл должен быть изображением');
+          return;
+        }
+
+        const reader = new FileReader();
+        reader.onload = (e) => {
+          if (typeof e.target?.result === 'string') {
+            const image = e.target.result;
+            imageSrc.value = image;
+            context.emit('uploadImage', image);
+            return;
+          }
+          console.error('не удалось загрузить изображение');
+        };
+        reader.readAsDataURL(file);
+      }
+    };
+
+    return {
+      loadImage,
+      imageInput,
+      imageSrc,
+    };
+  },
 });
 </script>
 
 <style lang="scss" scoped>
 .image-input {
+  position: relative;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -34,7 +83,23 @@ export default defineComponent({
     .image-input__label::before {
       transform: scale(1);
     }
+    .image-input__wrapper--with-image {
+      opacity: 1;
+    }
   }
+}
+.image-input__wrapper--with-image {
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.6);
+  opacity: 0;
+  transition: opacity 0.2s;
 }
 .image-input__label {
   position: relative;
@@ -55,6 +120,11 @@ export default defineComponent({
     transform: scale(0);
     transition: transform 0.2s;
   }
+}
+.image-input__preview {
+  height: 100%;
+  width: 100%;
+  object-fit: scale-down;
 }
 .image-input__input {
 }
