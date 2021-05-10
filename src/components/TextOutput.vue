d81d0421be74c4601762f00a90b4d3ba6c0c60ff
AlexOdnov <alex.odnoralov@yandex.ru>
2021-05-10 19:08:29 +0300

    add LoadingIndicator in TextOutput component

--- src/components/TextOutput.vue
+++ src/components/TextOutput.vue
@@ -1,42 +1,68 @@
 <template>
   <div class="text-output">
-    <p>
-      {{ recognizedText }}
-    </p>
+    <transition name="fade" mode="out-in">
+      <p class="text-output__recognized-text" v-if="recognizedText">
+        {{ recognizedText }}
+      </p>
+      <loading-indicator
+        v-else-if="!recognizedText && status"
+        :status="status"
+        :progress="progress"
+      />
+    </transition>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import LoadingIndicator from './LoadingIndicator.vue';
 
 export default defineComponent({
   name: 'TextOutput',
+
+  components: { LoadingIndicator },
 
   props: {
     recognizedText: {
       type: String,
       required: true,
-      default: '',
+    },
+    status: {
+      type: String,
+      required: true,
+    },
+    progress: {
+      type: Number,
+      required: true,
     },
   },
-
-  // setup() {},
 });
 </script>
 
 <style lang="scss" scoped>
 .text-output {
-  display: block;
   padding: var(--outer-padding);
-  overflow: auto;
-  text-align: left;
   background: var(--bgLightColor);
   border: 2px solid var(--color);
   border-radius: var(--outer-padding);
+}
+.text-output__recognized-text {
+  height: 100%;
+  text-align: left;
+  white-space: pre-wrap;
+  overflow: auto;
   scrollbar-width: none;
 
   &::-webkit-scrollbar {
     width: 0;
   }
+}
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.1s 0.2s;
 }
 </style>
