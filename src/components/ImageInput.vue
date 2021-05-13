7f1fda4daa418025e7beb847547d66f6cd288194
AlexOdnov <alex.odnoralov@yandex.ru>
2021-05-13 14:34:21 +0300

    implement drag&drop for image uploading

--- src/components/ImageInput.vue
+++ src/components/ImageInput.vue
@@ -1,11 +1,18 @@
 <template>
-  <label class="image-input">
+  <label
+    class="image-input"
+    :class="{ 'image-input--hovered': isHovered }"
+    @dragover.prevent="onDragover"
+    @dragleave.prevent="onDragleave"
+    @drop.stop.prevent="dropImage"
+  >
     <img class="image-input__preview" :src="imageSrc" v-show="imageSrc" />
     <span
       class="image-input__wrapper"
       :class="{ 'image-input__wrapper--with-image': imageSrc }"
     >
-      <span class="image-input__label">Загрузить файл</span>
+      <span class="image-input__label">Выберите или</span>
+      <span class="image-input__label">перетащите файл</span>
     </span>
     <input
       class="image-input__input visually-hidden"
@@ -30,6 +37,21 @@ export default defineComponent({
   setup(_, context) {
     const imageInput = ref<null | HTMLInputElement>(null);
     const imageSrc = ref<null | string>(null);
+    const isHovered = ref(false);
+
+    const readImage = (image: File) => {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        if (typeof e.target?.result === 'string') {
+          const b64Image = e.target.result;
+          imageSrc.value = b64Image;
+          context.emit('uploadImage', b64Image);
+          return;
+        }
+        console.error('не удалось загрузить изображение');
+      };
+      reader.readAsDataURL(image);
+    };
 
     const loadImage = () => {
       if (imageInput.value?.files?.length) {
@@ -39,25 +61,39 @@ export default defineComponent({
           console.error('файл должен быть изображением');
           return;
         }
+        readImage(file);
+      }
+    };
 
-        const reader = new FileReader();
-        reader.onload = (e) => {
-          if (typeof e.target?.result === 'string') {
-            const image = e.target.result;
-            imageSrc.value = image;
-            context.emit('uploadImage', image);
-            return;
-          }
-          console.error('не удалось загрузить изображение');
-        };
-        reader.readAsDataURL(file);
+    const onDragover = () => {
+      isHovered.value = true;
+    };
+
+    const onDragleave = () => {
+      isHovered.value = false;
+    };
+
+    const dropImage = (e: DragEvent) => {
+      isHovered.value = false;
+      if (e.dataTransfer?.files?.length) {
+        const file = e.dataTransfer.files[0];
+
+        if (!file.type.includes('image/')) {
+          console.error('файл должен быть изображением');
+          return;
+        }
+        readImage(file);
       }
     };
 
     return {
       loadImage,
+      onDragover,
+      onDragleave,
+      dropImage,
       imageInput,
       imageSrc,
+      isHovered,
     };
   },
 });
@@ -76,7 +112,8 @@ export default defineComponent({
   cursor: pointer;
 
   &:hover,
-  &:focus-within {
+  &:focus-within,
+  &--hovered {
     .image-input__label {
       opacity: 0.75;
     }
@@ -88,15 +125,18 @@ export default defineComponent({
     }
   }
 }
-.image-input__wrapper--with-image {
+.image-input__wrapper {
   position: absolute;
   top: 0;
   left: 0;
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
   width: 100%;
   height: 100%;
+}
+.image-input__wrapper--with-image {
   background: rgba(0, 0, 0, 0.6);
   opacity: 0;
   transition: opacity 0.2s;
@@ -104,7 +144,7 @@ export default defineComponent({
 .image-input__label {
   position: relative;
   padding: 2px 0;
-  font-size: 2rem;
+  font-size: 1.5rem;
   transition: opacity 0.2s;
 
   &::before {
