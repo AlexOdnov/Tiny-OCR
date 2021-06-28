3fecf43ee731863506c1b021b0bd2d29470902d0
AlexOdnov <alex.odnoralov@yandex.ru>
2021-06-28 21:46:47 +0300

    switch from google fonts to self-hosted font

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,6 @@ import App from './App.vue';
 import './registerServiceWorker';
 import './assets/normalize.scss';
 import './assets/base.scss';
+import './assets/font.scss';
 
 createApp(App).mount('#app');
